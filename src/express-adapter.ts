3c7529edfc02996ee4c03e1d83a895fbb3f05348
Augustus Kamau <kaugustus15@gmail.com>
2022-10-18 12:33:54 +0300

    revert(): revert sorting of imports

--- src/express-adapter.ts
+++ src/express-adapter.ts
@@ -1,7 +1,7 @@
-import { HttpAdapter, RequestHandler } from '@hemtypes/http-adapter';
 import * as http from 'http';
 import * as https from 'https';
 import * as express from 'express';
+import { HttpAdapter, RequestHandler } from '@hemtypes/http-adapter';
 
 export default class ExpressAdapter implements HttpAdapter {
   protected httpServer: any;
